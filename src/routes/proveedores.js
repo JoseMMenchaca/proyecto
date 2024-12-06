e8a910c280be3c142beafbab676f3f2a6f217574
ReACTor72 <rene.casilla@gmail.com>
2024-12-05 20:33:43 -0400

    Proveedores OK+
    Consultas general y por item, Baja, Nuevo + Actualización.

--- src/routes/proveedores.js
+++ src/routes/proveedores.js
@@ -38,19 +38,68 @@ router.post('/', (req, res) => {
 })
 
 //Baja al registro
+// router.put('/:id', (req, res) => {
+//     const { id } = req.params;
+//     //const { idProveedor, nombre, nombre_contacto, direccion, telefono, celular, email, estado } = req.body;
+//     _.each(proveedor, (xproveedor, i) => {
+//         console.log('?=', id, '---', i, proveedor[i].idProveedor, 'x=', xproveedor.idProveedor);
+//         console.log('?=', id);
+//         if (xproveedor.idProveedor == id) {
+//             console.log('____________________________________actualizado:  ', proveedor[i].idProveedor, xproveedor.nombre);
+//             proveedor[i].estado = "--Baja";
+//         }
+//     });
+//     res.send('-----dado de baja');
+// })
+
+//Actualizar registro
 router.put('/:id', (req, res) => {
     const { id } = req.params;
-    //const { idProveedor, nombre, nombre_contacto, direccion, telefono, celular, email, estado } = req.body;
+    const { idProveedor, nombre, nombre_contacto, direccion, telefono, celular, email, estado } = req.body;
+    //console.log(req.body);
+    console.log('-------------registro recibido :  ', idProveedor, nombre);
+
     _.each(proveedor, (xproveedor, i) => {
         console.log('?=', id, '---', i, proveedor[i].idProveedor, 'x=', xproveedor.idProveedor);
         console.log('?=', id);
         if (xproveedor.idProveedor == id) {
             console.log('____________________________________actualizado:  ', proveedor[i].idProveedor, xproveedor.nombre);
-            proveedor[i].estado = "--Baja";
+
+            if (nombre !== null && nombre !== undefined && nombre.trim() !== "") {
+                proveedor[i].nombre = nombre;
+                console.log('nombre');
+            }
+            if (nombre_contacto !== null && nombre_contacto !== undefined && nombre_contacto.trim() !== "") {
+                proveedor[i].nombre_contacto = nombre_contacto
+                console.log('contacto');
+            };
+            if (direccion !== null && direccion !== undefined && direccion.trim() !== "") {
+                proveedor[i].direccion = direccion
+                console.log('direccion');
+            };
+            if (telefono !== null && telefono !== undefined && telefono.trim() !== "") {
+                proveedor[i].telefono = telefono
+                console.log('telefono');
+            };
+            if (celular !== null && celular !== undefined && celular.trim() !== "") {
+                proveedor[i].celular = celular
+                console.log('celular');
+            };
+            if (email !== null && email !== undefined && email.trim() !== "") {
+                proveedor[i].email = email
+                console.log('emai');
+            };
+
+            if (estado !== null && estado !== undefined && estado.trim() !== "") {
+                //proveedor[i].estado = "--Baja";
+                proveedor[i].estado = estado;
+                console.log('estado');
+            }
         }
     });
-    res.send('-----dado de baja');
+    res.send('-----actualizado');
 })
+
 
 // Eliminar registro
 // router.delete('/:id', (req, res) => {
