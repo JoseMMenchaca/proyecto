e06352fbbbde60c9ec2fe76075322b13aa755506
JoseMMenchaca <49880557+JoseMMenchaca@users.noreply.github.com>
2024-12-09 17:40:30 -0400

    Create producto.controller.js

---
+++ src/controllers/producto.controller.js
@@ -0,0 +1,69 @@
+import { Producto } from "../models/Producto.js";
+
+export async function listarProductos( req, res){
+    try{
+        const productos=await Producto.findAll({
+            attributes:['id','nombre','descripcion','categoria_id'],
+        });
+        res.json(productos);
+    }catch(error){
+        res.status(500).json({
+            message:error.message,
+        });
+    }
+}
+
+export async function crearProducto(req, res){
+    const {nombre,descripcion,stock,categoria_id } = req.body;
+    try{
+        const newProducto=await Producto.create({
+            nombre,
+            descripcion,
+            stock,
+            categoria_id,
+            
+        },{
+            fields:['nombre','descripcion','stock','categoria_id',]
+        });
+        res.status(201).json(newProducto);
+    }catch(error){
+        res.status(500).json({
+            message:error.message,
+        });
+    }
+}
+
+export async function verProducto(req, res){
+    const {id}=req.params;
+    try{
+        const producto= await Producto.findOne({
+            where:{id}
+        });
+        res.status(201).json(producto);
+    }catch(error){
+        res.status(500).json({
+            message:error.message,
+        });
+    }
+}
+
+export async function actualizarProducto(req, res){
+    const {id}=req.params;
+    const {nombre, descripcion,stock,categoria_id} = req.body;
+
+    try{
+        const producto=await Producto.findByPk(id);
+        producto.nombre=nombre;
+        producto.descrpcion=descripcion;
+        producto.stock=stock;
+        producto.categoria_id=categoria_id;
+        await producto.save();
+        
+        res.status(201).json(producto);
+    }catch(error){
+        res.status(500).json({
+            message:error.message,
+        });
+    }
+}
+
