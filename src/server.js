e214687209303a39b941647f659c25e15a4f4196
Hide17298 <arnoldelgadocalle5@gmail.com>
2024-12-08 20:38:37 -0400

    4
    4

--- src/server.js
+++ src/server.js
@@ -1,20 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
 import { sequelize } from './database/db.js';
-//rutas
 import indexRoutes from './routes/index.js';
 import categoriaRoutes from './routes/categoria.routes.js';
 import clienteRoutes from './routes/cliente.routes.js';
 import proveedorRoutes from './routes/proveedor.routes.js';
-import express from "express";
-import morgan from "morgan";
-import { sequelize } from "./database/db.js";
-import indexRoutes from "./routes/index.js";
-import categoriaRoutes from "./routes/categoria.routes.js";
-import clienteRoutes from "./routes/cliente.router.js";
 import "./models/cliente.model.js";
-import proveedorRoutes from "./routes/proveedor.routes.js";
-
 // Crear la instancia de la aplicación
 const app = express();
 // Configuraciones
@@ -24,11 +15,6 @@ app.set("port", process.env.PORT || 3000); // Define el puerto para el servidor
 app.use(morgan("dev")); // Para logging de las solicitudes
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// Rutas
-app.use(indexRoutes);  // Usar rutas
-app.use('/api/categoria', categoriaRoutes);  // Rutas de categorías
-app.use('/api', clienteRoutes);
 
 app.use(indexRoutes); // Usar rutas
 app.use("/api/categoria", categoriaRoutes); // Rutas de categorías
@@ -55,17 +41,6 @@ sequelize
     console.error("Error al sincronizar las tablas:", error);
   });
 
-sequelize.sync({ alter: true })
-    .then(() => {
-        console.log("Tablas sincronizadas");
-    })
-    .catch((error) => {
-        console.error("Error al sincronizar las tablas:", error);
-    });
-
 app.listen(app.get('port'), () => {
     console.log(`Servidor corriendo en el puerto ${app.get('port')}`);
     });
-app.listen(app.get("port"), () => {
-  console.log(`Servidor corriendo en el puerto ${app.get("port")}`);
-});
