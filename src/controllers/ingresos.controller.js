bec79c859146963f75efa0392ef065a4b8e7dbdd
JoseMMenchaca <49880557+JoseMMenchaca@users.noreply.github.com>
2025-01-04 23:17:23 -0400

    Update ingresos.controller.js

--- src/controllers/ingresos.controller.js
+++ src/controllers/ingresos.controller.js
@@ -1,18 +1,41 @@
+import { sequelize } from "../database/db.js";
 import { Ingreso } from "../models/Ingresos.js";
+import { IngresoDetalle } from "../models/IngresoDetalles.js";
 
 // Función registrar nuevo ingreso
 export async function registrarIngreso(req, res) {
-  const { lote, cantidad, precio, precioVenta, saldoProducto } = req.body;
+  const t = await sequelize.transaction();
+
+  const { fechaIngreso, montoTotal, proveedor_id, detalles } = req.body;
 
   try {
     const newIngreso = await Ingreso.create({
-      lote,
-      cantidad,
-      precio,
-      precioVenta,
-      saldoProducto,
-    });
-    res.status(201).json(newIngreso);
+      fechaIngreso,
+      montoTotal,
+      proveedor_id,   
+    },
+    { transaction: t }
+  );
+
+   // Crear el post asociado al usuario
+   const Newdetalles = await IngresoDetalle.bulkCreate(
+    detalles.map(detalle => ({
+      producto_id: detalle.producto_id,
+      lote: detalle.lote,
+      cantidad: detalle.cantidad,
+      precio: detalle.precio,
+      precioVenta: detalle.precioVenta,
+      saldoProducto: detalle.saldoProducto,
+      ingreso_id: newIngreso.id // Asociamos cada post al usuario por su ID
+    })),{ transaction: t }
+  );
+   
+  // Si ambos registros se crean correctamente, confirmamos la transacción
+  await t.commit();
+
+    res.status(201).json({mensaje: 'Usuario y post creados correctamente',
+      newIngreso,
+      Newdetalles});
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
