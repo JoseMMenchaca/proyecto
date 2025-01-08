97ca4de8020a031e86747ff579e62ed43835c4eb
Hide17298 <arnoldelgadocalle5@gmail.com>
2025-01-07 23:55:20 -0400

    modificaciones generales en INGRESOS
    INGRESOS

--- src/controllers/ingresos.controller.js
+++ src/controllers/ingresos.controller.js
@@ -1,43 +1,69 @@
 import { sequelize } from "../database/db.js";
 import { Ingreso } from "../models/Ingresos.js";
 import { IngresoDetalle } from "../models/IngresoDetalles.js";
+import { Proveedor } from "../models/Proveedor.js"
+
+// Función listar todos los ingresos
+export async function listarIngresos(req, res) {
+  try {
+    const ingresos = await Ingreso.findAll({
+    
+      include: [
+      {
+        model: Proveedor, // El modelo de la Proveedor
+        as: "proveedor",
+        attributes: ["id", "nombre"], // Atributos que deseas incluir de la Proveedor
+      },
+    ],
+  });
+    res.json(ingresos);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+}
 
 // Función registrar nuevo ingreso
 export async function registrarIngreso(req, res) {
   const t = await sequelize.transaction();
 
-  const { fechaIngreso, montoTotal, proveedor_id, detalles } = req.body;
+  const { fechaIngreso, proveedor_id, detalles} = req.body;
+
+  // Calcular montoTotal basado en los detalles
+  const montoTotal = detalles.reduce((acc, detalle) => acc + detalle.total, 0);
 
   try {
     const newIngreso = await Ingreso.create({
       fechaIngreso,
-      montoTotal,
-      proveedor_id,   
-    },
-    { transaction: t }
-  );
+      montoTotal, // Aquí aseguramos que el montoTotal se pase
+      proveedor_id,
+    }, { transaction: t });
 
-   // Crear el post asociado al usuario
-   const Newdetalles = await IngresoDetalle.bulkCreate(
-    detalles.map(detalle => ({
-      producto_id: detalle.producto_id,
-      lote: detalle.lote,
-      cantidad: detalle.cantidad,
-      precio: detalle.precio,
-      precioVenta: detalle.precioVenta,
-      saldoProducto: detalle.saldoProducto,
-      ingreso_id: newIngreso.id // Asociamos cada post al usuario por su ID
-    })),{ transaction: t }
-  );
-   
-  // Si ambos registros se crean correctamente, confirmamos la transacción
-  await t.commit();
+    const Newdetalles = await IngresoDetalle.bulkCreate(
+      detalles.map(detalle => ({
+        producto_id: detalle.producto_id,
+        // lote: detalle.lote,
+        cantidad: detalle.cantidad,
+        precio: detalle.precio,
+        precioVenta: detalle.precioVenta,
+        saldoProducto: detalle.saldoProducto,
+        ingreso_id: newIngreso.id,
+      })),
+      { transaction: t }
+    );
 
-    res.status(201).json({mensaje: 'Usuario y post creados correctamente',
+    await t.commit();
+
+    res.status(201).json({
+      mensaje: 'Ingreso y detalles creados correctamente',
       newIngreso,
-      Newdetalles});
+      Newdetalles
+    });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    await t.rollback();
+    console.error('Error al crear ingreso:', error);
+    res.status(500).json({ message: 'Error al guardar el ingreso', error: error.message });
   }
 }
 
@@ -71,16 +97,6 @@ export async function actualizarSaldo(req, res) {
     await ingreso.save();
 
     res.json(ingreso);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-}
-
-// Función listar todos los ingresos
-export async function listarIngresos(req, res) {
-  try {
-    const ingresos = await Ingreso.findAll();
-    res.json(ingresos);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
